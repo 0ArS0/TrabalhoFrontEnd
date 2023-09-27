11f9895fa0ce0e2815d5d3209e0f86e258de1fa1
ArthurM <arthurmonteiro20172018@gmail.com>
2023-09-27 14:42:33 -0300

    Melhorando a pagina produtos, adicionando botao de compra, descricao para produtos, renovando imagens, assim como adicionando um menu de compra

---
+++ js/script.js
@@ -0,0 +1,41 @@
+// Exibicao o modal de compra
+function mostrarModalCompra(produtoId) {
+  const modal = document.getElementById("modalCompra");
+  modal.style.display = "block";
+
+  // Fechamento do modal
+  const closeButton = document.querySelector(".close");
+  closeButton.addEventListener("click", () => {
+    modal.style.display = "none";
+  });
+
+  // Adiciona o produto ao carrinho
+  const adicionarCarrinhoButton = document.getElementById("adicionarCarrinho");
+  adicionarCarrinhoButton.addEventListener("click", () => {
+    const quantidade = document.getElementById("quantidade").value;
+    alert(
+      `Produto ${produtoId} adicionado ao carrinho com ${quantidade} unidades.`
+    );
+    modal.style.display = "none";
+  });
+
+  // Feche o modal se o usuário clicar fora dele
+  window.addEventListener("click", (event) => {
+    if (event.target === modal) {
+      modal.style.display = "none";
+    }
+  });
+}
+
+// Evento de clique para os botões "Comprar"
+const botoesComprar = document.querySelectorAll("button[data-id]");
+botoesComprar.forEach((botao) => {
+  botao.addEventListener("click", (event) => {
+    const produtoId = event.target.getAttribute("data-id");
+    mostrarModalCompra(produtoId);
+  });
+});
+
+
+
+
